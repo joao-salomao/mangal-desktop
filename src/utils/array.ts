c6958bb93d75f66612eb4c7bfbdc84153ea64296
João Salomão <joaosalomao.mcp@gmail.com>
2024-11-02 13:10:33 -0300

    Create new array utility to group by specific keys

--- src/utils/array.ts
+++ src/utils/array.ts
@@ -15,3 +15,18 @@ export function chunk<T>(array: T[], size: number): T[][] {
 export function pluck<T, K extends keyof T>(array: T[], key: K): T[K][] {
     return array.map(item => item[key])
 }
+
+/**
+ * It returns a new hash map with the values of the specified key from the original array as keys.
+ * Example: groupBy([{id: 1, name: 'John'}, {id: 2, name: 'Jane'}], 'id') => {1: {id: 1, name: 'John'}, 2: {id: 2, name: 'Jane'}}
+ */
+export function groupBy<T, K extends keyof T>(array: T[], key: K): Record<T[K], T[]> {
+    return array.reduce((acc, item) => {
+        const group = item[key]
+        if (!acc[group]) {
+            acc[group] = []
+        }
+        acc[group].push(item)
+        return acc
+    }, {} as Record<T[K], T[]>)
+}
