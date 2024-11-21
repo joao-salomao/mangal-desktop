0d31f084a3fe8fb4438909d349c0e2eea54f0e03
João Salomão <joaosalomao.mcp@gmail.com>
2024-11-21 20:13:06 -0300

    Removing unnecessary logs

--- src/services/keyValueDatabaseService.ts
+++ src/services/keyValueDatabaseService.ts
@@ -4,15 +4,12 @@ import {Store} from '@tauri-apps/plugin-store'
 let store: Store | null = null
 
 export async function get<T>(key: string): Promise<T | null> {
-    logger.info(`KeyValueDatabase: Get value for key "${key}"`)
     const store = await getStore()
     const value = await store.get<T | null | undefined>(key)
     return value ?? null
 }
 
-
 export async function set<T>(key: string, value: T): Promise<void> {
-    logger.info(`KeyValueDatabase: Set value for key "${key}": ${value}`)
     const store = await getStore()
     await store.set(key, value)
 }
