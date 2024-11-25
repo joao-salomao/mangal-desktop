0a18e136434d0fe4a8ada5aad32d24ad089f3a34
João Salomão <joaosalomao.mcp@gmail.com>
2024-11-25 01:29:26 -0300

    Add script to install Tauri Debian required dependencies

---
+++ scripts/install_tauri_debian_dependencies.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+# This script install the Tauri Debian required dependencies
+sudo apt update
+sudo apt install libwebkit2gtk-4.1-dev curl build-essential wget file libxdo-dev libssl-dev libayatana-appindicator3-dev librsvg2-dev
